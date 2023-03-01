a59069ef9294213d0f7e7ea5cbfd31d8709ae018
madsemilf <55183287+Madsemilf@users.noreply.github.com>
2023-03-01 09:18:01 +0100

    created fetch events for API

--- _app/js/modules/fetchData.js
+++ _app/js/modules/fetchData.js
@@ -1,4 +1,30 @@
-export default function fetchData() {
+export default async function fetchData() {
 
-	
+	const searchElement = document.getElementById('event-search');
+	const eventOutput = document.getElementById('event-output');
+	const resultElement = document.getElementById('result');
+	const errorElement = document.getElementById('error');
+
+	async function fetchEvents() {
+		const baseUrl = 'https://app.ticketmaster.com/discovery/v2/events.json';
+		const apiKey = 'w7baiDCMhxZVZXRxEGfihdDaXSIVFcZB';
+		const countryCode = `NO`
+		const options = {
+			method: 'GET',
+			headers: {
+				size: 100
+			}
+		}
+
+		const endpointNorway = `${baseUrl}?apikey=${apiKey}&countryCode=${countryCode}&`;
+
+		const response = await fetch(endpointNorway);
+		const events = await response.json()
+		console.log(events._embedded.events)
+	}
+
+	await fetchEvents();
+
+
 }
+
