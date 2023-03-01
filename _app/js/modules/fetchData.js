474875287b3dfbaf6fdff23ea48d582be753619a
madsemilf <55183287+Madsemilf@users.noreply.github.com>
2023-03-01 11:17:21 +0100

    added encryption to API

--- _app/js/modules/fetchData.js
+++ _app/js/modules/fetchData.js
@@ -1,30 +1,47 @@
+import { e } from "./env.js"
 export default async function fetchData() {
 
 	const searchElement = document.getElementById('event-search');
-	const eventOutput = document.getElementById('event-output');
 	const resultElement = document.getElementById('result');
 	const errorElement = document.getElementById('error');
+	let events = []
 
 	async function fetchEvents() {
 		const baseUrl = 'https://app.ticketmaster.com/discovery/v2/events.json';
-		const apiKey = 'w7baiDCMhxZVZXRxEGfihdDaXSIVFcZB';
-		const countryCode = `NO`
+		const countryCode = 'NO'
+		const musicGenre = 'KZFzniwnSyZfZ7v7nJ'
 		const options = {
 			method: 'GET',
 			headers: {
-				size: 100
+				genre: {
+					name: "Dance/Electronic"
+				}
 			}
 		}
 
-		const endpointNorway = `${baseUrl}?apikey=${apiKey}&countryCode=${countryCode}&`;
+		const endpointNorway = `${baseUrl}?apikey=${e}&countryCode=${countryCode}&genreId=${musicGenre}&`;
 
 		const response = await fetch(endpointNorway);
-		const events = await response.json()
-		console.log(events._embedded.events)
+		const data = await response.json();
+		events = data._embedded.events;
+		console.log(events);
+	}
+
+	async function searchEvents() {
+		const query = document.getElementById('search-input').value;
+
+		const filteredEvents = events.filter(event => {
+			const eventName = event.name.toLowerCase();
+			return eventName.includes(query.toLowerCase());
+		});
+
+		console.log(filteredEvents)
+		
 	}
 
 	await fetchEvents();
-
+	searchElement.addEventListener('click', searchEvents);
+	
 
 }
 
