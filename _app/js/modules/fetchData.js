e1579a0d021fd909d1472658c91e28b263a57a05
madsemilf <55183287+Madsemilf@users.noreply.github.com>
2023-05-30 17:01:39 +0200

    Fixed searchEvents function to push elements to DOM

--- _app/js/modules/fetchData.js
+++ _app/js/modules/fetchData.js
@@ -6,20 +6,24 @@ export default async function fetchData() {
 	const errorElement = document.getElementById('error');
 	let events = []
 
+	resultElement.classList.add('events'); 
+
 	async function fetchEvents() {
 		const baseUrl = 'https://app.ticketmaster.com/discovery/v2/events.json';
 		const countryCode = 'NO'
 		const musicGenre = 'KZFzniwnSyZfZ7v7nJ'
+		const size = '100';
 		const options = {
 			method: 'GET',
 			headers: {
+				size: 100,
 				genre: {
 					name: "Dance/Electronic"
 				}
 			}
 		}
 
-		const endpointNorway = `${baseUrl}?apikey=${e}&countryCode=${countryCode}&genreId=${musicGenre}&`;
+		const endpointNorway = `${baseUrl}?apikey=${e}&countryCode=${countryCode}&${size}`;
 
 		const response = await fetch(endpointNorway);
 		const data = await response.json();
@@ -29,19 +33,36 @@ export default async function fetchData() {
 
 	async function searchEvents() {
 		const query = document.getElementById('search-input').value;
-
+  
 		const filteredEvents = events.filter(event => {
-			const eventName = event.name.toLowerCase();
-			return eventName.includes(query.toLowerCase());
+			const city = event._embedded.venues[0].city.name.toLowerCase();
+			return city.includes(query.toLowerCase());
 		});
+  
+		resultElement.innerHTML = ''; // Clear previous results
+  
+		for (let i = 0; i < Math.min(filteredEvents.length, 5); i++) {
+		  const event = filteredEvents[i];
+		  const container = document.createElement('div');
+		  container.classList.add('events');
 
-		console.log(filteredEvents)
-		
-	}
+		  const eventNameElement = document.createElement('h3');
+		  eventNameElement.textContent = event.name;
+    	  container.appendChild(eventNameElement);
 
-	await fetchEvents();
-	searchElement.addEventListener('click', searchEvents);
-	
+			const eventImageElement = document.createElement('img');
+			eventImageElement.src = event.images[0].url;
+			container.appendChild(eventImageElement);
+  
+		  resultElement.appendChild(container);
+		}
+	 }
+  
+	 await fetchEvents();
+	 searchElement.addEventListener('click', searchEvents);
+  }
+  
+  fetchData();
 
-}
+
 
